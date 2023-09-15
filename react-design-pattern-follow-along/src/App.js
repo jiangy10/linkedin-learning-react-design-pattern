2b748a47c98de786d334bae82b36e55a7be9aca9
JiangFanGang <jiangy10@rose-hulman.edu>
2023-09-14 20:23:45 -0400

    split-screen component

--- react-design-pattern-follow-along/src/App.js
+++ react-design-pattern-follow-along/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {SplitScreen} from "./SplitScreen";
+
+const leftComponent = () =>{
+    return (<h1 style={{backgroundColor: "blue"}}>Left!</h1>);
+}
+
+const rightComponent = () =>{
+    return (<p>Right!</p>)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplitScreen left ={leftComponent} right = {rightComponent}></SplitScreen>
     </div>
   );
 }
