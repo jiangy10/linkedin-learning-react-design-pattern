287d19a8dbf4d5b2a92cf6da35cb0d382f6321ec
JiangFanGang <jiangy10@rose-hulman.edu>
2023-09-14 20:39:47 -0400

    split-screen improvement

--- react-design-pattern-follow-along/src/App.js
+++ react-design-pattern-follow-along/src/App.js
@@ -2,18 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import {SplitScreen} from "./SplitScreen";
 
-const leftComponent = () =>{
+const LeftComponent = () =>{
     return (<h1 style={{backgroundColor: "blue"}}>Left!</h1>);
 }
 
-const rightComponent = () =>{
+const RightComponent = () =>{
     return (<p>Right!</p>)
 }
 
 function App() {
   return (
     <div className="App">
-      <SplitScreen left ={leftComponent} right = {rightComponent}></SplitScreen>
+      <SplitScreen leftWeight={4} rightWeight={1}>
+          <LeftComponent/>
+          <RightComponent/>
+      </SplitScreen>
     </div>
   );
 }
